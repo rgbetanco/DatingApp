909e911b918f1a3d8dd21e60969f1cda76c625c5
Ronald <rgbetanco@gmail.com>
2019-02-08 12:05:33 +0800

    End of section 12

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -26,6 +26,7 @@ namespace DatingApp.API.Helpers
             CreateMap<UserForUpdateDto, User>();
             CreateMap<Photo, PhotoForReturnDto>();
             CreateMap<PhotosForCreationDto, Photo>();
+            CreateMap<UserForRegisterDto, User>();
         }
     }
 }
